36416054dc802b99de3b6c39447d8bbd3090e242
Aubrey Neimeier <neimeier.1@osu.edu>
2017-12-20 13:08:46 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,10 +16,11 @@ def long_planeteer_calls(calls)
   calls.any? {|call| call.length > 4}
 end
 
-def find_the_cheese(ingredients)
-  cheeses = ["gouda", "cheddar", "camembert"]
-  ingredients.find do |cheese|
-    cheese.include?(cheeses)
+def find_the_cheese(potentially_cheesy_items)
+  cheeses = %w(gouda cheddar camembert)
+
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
   end
 end
 
