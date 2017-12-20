4eabeda195d137c02ce342c9f9a7be4df0d76506
Aubrey Neimeier <neimeier.1@osu.edu>
2017-12-20 13:09:29 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,6 +16,7 @@ def long_planeteer_calls(calls)
   calls.any? {|call| call.length > 4}
 end
 
+#formal solution...DO NOT UNDERSTAND!!
 def find_the_cheese(potentially_cheesy_items)
   cheeses = %w(gouda cheddar camembert)
 
@@ -24,8 +25,7 @@ def find_the_cheese(potentially_cheesy_items)
   end
 end
 
-
-
+#my solution is very tedious
 =begin
 def find_the_cheese(recipe)
   #cheeses = ["cheddar", "gouda", "camembert"]
